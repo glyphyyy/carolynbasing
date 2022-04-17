a5ad9dca6c0e6fb82a3f4984ebe7803275413707
glyphyyy <93383520+glyphyyy@users.noreply.github.com>
2022-04-17 20:13:37 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename="/React">
       <App />
     </HashRouter>
   </React.StrictMode>,
